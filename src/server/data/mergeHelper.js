ed239106f80d49bfd676d484f6fb2c16d114c757
Doug <douglaslubaway@gmail.com>
2020-12-08 14:18:53 -0500

    Continued Progression Towards "Fixing"
    Overview
    More progress made with ESLint changes.
    
    Data Model / DB Schema Changes
    N/A
    
    Environment / Configuration Changes
    N/A
    
    Notes
    End of Day.

--- src/server/data/mergeHelper.js
+++ src/server/data/mergeHelper.js
@@ -1,38 +1,38 @@
-const fs = require('fs');
+﻿const fs = require('fs');
 
 const allLearners = [];
 
 const buildLearners = () => {
-    fs.readdir('./src/server/data/learners', 'utf8', (error, files) => {
-        if (error) {
-            throw error;
-        }
+  fs.readdir('./src/server/data/learners', 'utf8', (error, files) => {
+    if (error) {
+      throw error;
+    }
 
-        let count = 0;
-        const increaseCount = 1;
+    let count = 0;
+    const increaseCount = 1;
 
-        files.forEach(file => {
-            const data = require(`./learners/${file}`);
+    files.forEach(file => {
+      const data = require(`./learners/${file}`);
 
-            data.id = count;
-            count += increaseCount;
-            data.projects.forEach(project => {
-                project.id = count;
-                count += increaseCount;
-            });
-            data.skills.forEach(skill => {
-                skill.id = count;
-                count += increaseCount;
-            });
-            data.experience.forEach(exp => {
-                exp.id = count;
-                count += increaseCount;
-            });
-            allLearners.push(data);
+      data.id = count;
+      count += increaseCount;
+      data.projects.forEach(project => {
+        project.id = count;
+        count += increaseCount;
+      });
+      data.skills.forEach(skill => {
+        skill.id = count;
+        count += increaseCount;
+      });
+      data.experience.forEach(exp => {
+        exp.id = count;
+        count += increaseCount;
+      });
+      allLearners.push(data);
 
-return allLearners;
-        });
+      return allLearners;
     });
+  });
 };
 
 buildLearners();
